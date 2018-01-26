2589d2e90ee5d90329008e2aa3ca3dadada40a30
Brownbull <carcamo.gabriel@gmail.com>
2018-01-25 22:10:25 -0300

    front end done

---
+++ AppDev.sh
@@ -0,0 +1,30 @@
+# First imagine how the app will look like
+## define things in display like logo, buttons, inputs and messages
+
+# work in create components
+# then create app
+$ create-react-app appName
+# go directory
+$ cd appName
+
+# delete some components
+ # appName\src\logo.svg
+ # remove dependency on app.js and all html defined on App
+
+# Add components to create on App
+
+# Create folder components
+$ mkdir components
+
+# Start create folder and files for components
+
+# Once creates must import those in App.js
+
+# if you need style intall tachyons (must have a cmd in admin mode open)
+$ npm install tachyons
+
+# add import to index.js
+import 'tachyons';
+
+
+
---
+++ facerecognitionbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Gabriel, Your current rank is ...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    ); //eof return
+} //eof const Logo
+
+export default Logo;
