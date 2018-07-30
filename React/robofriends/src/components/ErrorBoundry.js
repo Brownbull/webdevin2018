d1895c5f32449a235fabe27ccae4c7d08d74bfcc
Brownbull <carcamo.gabriel@gmail.com>
2018-07-30 15:39:57 -0400

    done robofriends with localData

---
+++ React/robofriends/src/components/ErrorBoundry.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class ErrorBoundry extends Component {
+constructor(props){
+    super(props)
+    this.state = {
+      hasError: false
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Not Good!</h1>
+    }
+    return this.props.children
+  }
+}
